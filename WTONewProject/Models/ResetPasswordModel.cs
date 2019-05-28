db58bc9bd468dee1155dd8076bc9a45892c6ea27
StephenYu <azura3d@163.com>
2019-05-28 16:22:52 +0800

    修改密码完成

---
+++ WTONewProject/Models/ResetPasswordModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WTONewProject.Models
+{
+    public class ResetPasswordModel
+    {
+        public bool success { get; set; }
+        public Result result { get; set; }
+    }
+
+
+
+    public class Result
+    {
+        public bool successed { get; set; }
+        public string error { get; set; }//登录账号
+
+    }
+}
--- WTONewProject/Tools/Constants.cs
+++ WTONewProject/Tools/Constants.cs
@@ -12,6 +12,8 @@ namespace WTONewProject.Tools
         public static string URL_WEB = URL_ROOT + "/Mobile/index";
         public static string URL_GET_USER = URL_ROOT + "/api/Account/GetUserInfo";
         public static string URL_GET_ACCESSTOKEN = URL_ROOT + "api/TokenAuth/Authenticate";//登陆
+        public static string URL_GET_IDENTIFYINGCODE = URL_ROOT + "api/services/app/User/SendIdentifyingCode";//获取验证码
+        public static string URL_RESETOWNPASSWORD = URL_ROOT + "api/services/app/User/ResetOwnPassword";//重新设置密码
 
         public static string STORAGE_TYPE_DOC = "document";
         public static string STORAGE_TYPE_DOWNLOAD = "download";
